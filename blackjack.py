0735dfc7cd31ba20f5eabdcbd8e7bd6ae6f09840
VashTX <ricardo.reyesjr@hotmail.com>
2022-01-27 11:30:30 -0600

    commit 1 v1.0.1

--- blackjack.py
+++ blackjack.py
@@ -1,22 +1,19 @@
 import time
 import random
 
-'''Purpose: -- BlackJack game the goal is to reach 21 with out going over the player that is closest wins. --
-
-Constraints: -- Players use a poker set of card A-K
-                A = 1 or 11
-                10, J, Q, K = 10
-                2 to 9 = the keep their face value --
-
-Expected Results: -- two player game one player wins by getting 21 or the player that is closest to 21 wins. --
-
-
-Version		Author		   Date		          Description
----------------------------------------------------------------
-1.0         Rick Reyes	  16 Jan. 2022	         Blackjack game.
-'''
-# The value of the A's is 11 or 1.
-# a = 1 or 11
+# Purpose: -- BlackJack game the goal is to reach 21 with out going over the player that is closest wins. --
+#
+# Constraints: -- Players use a poker set of card A-K
+#              A = 1 or 11
+#                10, J, Q, K = 10
+#                2 to 9 = the keep their face value --
+#
+# Expected Results: -- two player game one player wins by getting 21 or the player that is closest to 21 wins. --
+#
+#
+# Version		Author		   Date		          Description
+# ---------------------------------------------------------------
+# 1.0         Rick Reyes	  16 Jan. 2022	         Blackjack game.
 
 # Intro
 print('          ♠♣♦♥♠♣♦♥♠♣♦♥♠♣♦♥♠♣♦♥♠♣♦♥♠')
@@ -37,9 +34,7 @@ if play_game == 'n' or play_game == 'N':
     print("Guess BlackJack isn't for you maybe next time.")
 elif play_game == 'y' or play_game == 'Y':
     print()
-    print('The goal of the game is to reach a total of 21 using a set of poker cards')
-    time.sleep(2)
-    print('The value of each card is as follows: A = 1 or 11 | 10, J, Q, K = 10 | 2 - 9 are face value')
+    print('The goal of the game is to reach a total of 21.')
     time.sleep(2)
     print("It's you vs the dealer, in order to win you need to be closest to 21 or get 21 if your over you bust.\n")
     time.sleep(2)
